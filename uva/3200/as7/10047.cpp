7603fb0c5f51571f60afe03e478c7db22a854780
strinsberg <stevendkom@gmail.com>
2020-02-27 20:29:05 -0700

    Added some 3200 uva problems.

--- uva/3200/as7/10047.cpp
+++ uva/3200/as7/10047.cpp
@@ -11,7 +11,7 @@ int calc_val(int pos, int x) {
 int main() {
   int m,n,size,start,time, c=1;
 
-  while (cin >> n >> m and n) {
+  while (cin >> m >> n and m) {
     time = 1e9;
     size = n*m*20;
     vector<vector<int>> G(size);
@@ -47,40 +47,41 @@ int main() {
         int pos = cell+i;  // The node number
 
         // Add internal connections
-        if (i < 15) {
+        if (i >= 0 and i < 5) {
           G[pos].push_back(pos + 5);
           G[pos + 5].push_back(pos);
-        } else if (i < 10) {
-          G[pos].push_back(pos + 10);
-          G[pos + 10].push_back(pos);
-        } else if (i < 5) {
           G[pos].push_back(pos + 15);
           G[pos + 15].push_back(pos);
-        }        
+        } else if (i >= 10 and i < 15) {
+          G[pos].push_back(pos - 5);
+          G[pos - 5].push_back(pos);
+          G[pos].push_back(pos + 5);
+          G[pos + 5].push_back(pos);
+        }
 
         // Add External connections
         int val;
         if (i < 5) {  // Facing N
-          val = calc_val(pos, (pos - n * 20) + 1);
+          val = calc_val(pos, (pos - (n * 20)) + 1);
           if (val >= 0) {
             G[pos].push_back(val);
           }
 
         } else if (i < 10) {  // Facing E
           val = calc_val(pos, (pos + 20) + 1);
-          if (k % m != m - 1) {
+          if (k % n != n - 1) {
             G[pos].push_back(val);
           }
 
         } else if (i < 15) {  // Facing S
-          val = calc_val(pos, (pos + n * 20) + 1);
+          val = calc_val(pos, (pos + (n * 20)) + 1);
           if (val < size) {
             G[pos].push_back(val);
           }
 
         } else {  // Facing W
           val = calc_val(pos, (pos - 20) + 1);
-          if (k % m != 0) {
+          if (k % n != 0) {
             G[pos].push_back(val);
           }
         }
@@ -90,18 +91,20 @@ int main() {
     // Do the bfs
     queue<pair<int, int>> q;
     q.emplace(start, 0);
+    visited[start] = 1;
     while (!q.empty()) {
       pair<int, int> p = q.front();
       q.pop();
-      visited[p.first] = 1;
 
       if (end.count(p.first)) {
         time = min(time, p.second);
+        break;
       }
 
       for (auto i : G[p.first]) {
         if (!visited[i]) {
           q.emplace(i, p.second+1);
+          visited[i] = 1;
         }
       }
     }
