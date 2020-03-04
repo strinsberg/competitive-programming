5a9cd596881c7a79e58a121539f5007a41e7edd7
strinsberg <stevendkom@gmail.com>
2020-03-04 12:16:17 -0700

    Added two new files for the 3200 project.

---
+++ uva/3200/proj/2019/C.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// We can build a small graph for the cube and just check the connections
+// against the sides of a cube. Start at position 0 and if you find a connection
+// that would not be possible return false. Otherwise it is true.
+
+
+int main() {
+
+  return 0;
+}
---
+++ uva/3200/proj/2019/H.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// For each triple of lines. Find the 3 intersection points and use them
+// to calculate the length of the sides. Add these together and get the
+// perimeter. Keep track of the max value. If the perimeter is -1 at the end
+// there is no triangle possible. Probably need howards line intersection
+// code
+
+int main() {
+
+  return 0;
+}
